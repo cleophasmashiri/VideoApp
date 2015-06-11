702b37fec3a8a8793c39fee5390583234499f940
cleophas mashiri <cleophasmashiri@gmail.com>
2015-06-11 20:05:06 +0200

    add movie controller

--- VideoApp.Web/Startup.cs
+++ VideoApp.Web/Startup.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Routing;
 using Microsoft.Framework.DependencyInjection;
 
 namespace VideoApp.Web
@@ -13,14 +15,12 @@ namespace VideoApp.Web
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc();
         }
     }
 }
